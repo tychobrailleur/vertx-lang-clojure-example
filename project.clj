ea6f39dd2a9a8070ab8b22726f7fa063a02692e5
Sébastien Le Callonnec <sebastien@weblogism.com>
2019-05-26 12:00:05 +0100

    Update project details.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject vertx-lang-clojure-example "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Vert.x Clojure Example"
+  :url "https://github.com/tychobrailleur/vertx-lang-clojure-example"
+  :license {:name "Apache License 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [io.vertx/vertx-web "3.7.0"]
                  [com.w2v4/vertx-lang-clojure "3.7.0-SNAPSHOT"]]
