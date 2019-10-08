fe20292d06982962612de0a79ca77c8576aedc0c
Sébastien Le Callonnec <sebastien@weblogism.com>
2019-10-08 21:19:18 +0100

    Update example with latest version.

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Apache License 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [com.w2v4/vertx-lang-clojure "3.7.1-SNAPSHOT"]]
+                 [com.w2v4/vertx-lang-clojure "3.8.2-SNAPSHOT"]]
   :main vertx-lang-clojure-example.core)
