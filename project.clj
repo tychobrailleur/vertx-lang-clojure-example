d8638910e7cc0cf4a406645c3211c17e1da2f59c
Sébastien Le Callonnec <sebastien@weblogism.com>
2019-10-08 09:25:42 +0100

    Remove unneeded dependency.

--- project.clj
+++ project.clj
@@ -4,6 +4,5 @@
   :license {:name "Apache License 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [io.vertx/vertx-web "3.7.1"]
                  [com.w2v4/vertx-lang-clojure "3.7.1-SNAPSHOT"]]
   :main vertx-lang-clojure-example.core)
