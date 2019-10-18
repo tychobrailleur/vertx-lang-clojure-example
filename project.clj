2d3e8b6866f847e1ea121fd9def7037bbea9e621
Sébastien Le Callonnec <sebastien@weblogism.com>
2019-10-18 19:06:45 +0100

    Update to Vert.x 3.8.3.

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Apache License 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [io.vertx/vertx-web "3.8.2"]
-                 [io.vertx/vertx-config "3.8.2"]
+                 [io.vertx/vertx-web "3.8.3"]
+                 [io.vertx/vertx-config "3.8.3"]
                  [org.slf4j/slf4j-api "1.7.28"]
                  [ch.qos.logback/logback-core "1.2.3"]
                  [ch.qos.logback/logback-classic "1.2.3"]
-                 [com.w2v4/vertx-lang-clojure "3.8.2-SNAPSHOT"]]
+                 [com.w2v4/vertx-lang-clojure "3.8.3-SNAPSHOT"]]
   :aot [vertx-lang-clojure-example.core]
   :env-vars [".env-vars"]
   :plugins [[lein-with-env-vars "0.2.0"]]
