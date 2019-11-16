462f14db9de9f86f2ef444ac9f317b4613d87f19
Sébastien Le Callonnec <sebastien@weblogism.com>
2019-11-16 15:55:34 +0000

    4.0.0 implementation.

--- src/vertx_lang_clojure_example/core.clj
+++ src/vertx_lang_clojure_example/core.clj
@@ -1,10 +1,12 @@
 (ns vertx-lang-clojure-example.core
-  (:require [io.vertx.clojure.core.vertx :as vertx])
+  (:require [io.vertx.clojure.core.vertx :as vertx]
+            [io.vertx.clojure.core.vertx-options :as vertx-options])
    (:gen-class))
 
 (defn -main [& args]
   (let [v (vertx/vertx)]
-    (vertx/deploy-verticle v "vertx-lang-clojure-example.websocket.clj")
-    (vertx/deploy-verticle v "vertx-lang-clojure-example.http_server.clj")
-    (vertx/deploy-verticle v "vertx-lang-clojure-example.net-server.clj")
-    (vertx/deploy-verticle v "vertx-lang-clojure-example.sql.clj")))
+    ;; Need to figure out how to handle overloaded `deployVerticle`
+    (.deployVerticle v "vertx-lang-clojure-example.websocket.clj")
+    (.deployVerticle v "vertx-lang-clojure-example.http_server.clj")
+    (.deployVerticle v "vertx-lang-clojure-example.net-server.clj")
+    (.deployVerticle v "vertx-lang-clojure-example.sql.clj")))
